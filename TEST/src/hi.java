da8e046a4720e0bd174d1e8bb03b24ccdd72a2b4
이성혁 <@LAPTOP-NRGQL6C7>
2022-02-03 20:46:25 +0900

    git 연동 끝

---
+++ TEST/src/hi.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+/**
+ * @author ¿Ãº∫«ı
+ *
+ */
+
+public class hi {
+    public static void numbering() {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+        
+        
+        System.out.println("FINISH");
+    }
+ 
+    public static void main(String[] args) {
+        numbering();
+    }
+}
+ 
